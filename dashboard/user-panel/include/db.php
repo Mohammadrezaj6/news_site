716fa2032cc3936dc0c81b4ab116effd75d695e8
Mamadreza <mamareza.j6@gamil.com>
2025-04-30 16:13:17 +0330

    add file  for gh-pages

--- dashboard/user-panel/include/db.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$db = new PDO(DNS, DB_USER, DB_PASS);
--- include/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define("DNS", "mysql:host=localhost;dbname=news_site_db;charset=utf8mb4");
-
-define("DB_USER", "root");
-
-define("DB_PASS", "");
